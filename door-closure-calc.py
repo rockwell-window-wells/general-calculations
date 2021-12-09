4ecd027771f2190dcedf985f2c74ac6bba6f2675
ryantheengineer <ryan.larson.wa@gmail.com>
2021-12-09 09:35:38 -0700

    Updated to calculate using an ideal spring for the cable closure

--- door-closure-calc.py
+++ door-closure-calc.py
@@ -11,31 +11,32 @@ import matplotlib.pyplot as plt
 
 ########## Torsion spring option ##########
 # Initialize angles in degrees for open position
-min_open = 0.0
+min_open = 1.0
 max_open = 90.0
-npts = 91
+npts = 90
 theta_open = np.linspace(min_open, max_open, num=npts, endpoint=True)
 
 # print(theta_open)
 
 # Set parameters
-theta_dead = 0.0   # Angle at which the springs start to engage
+theta_dead = 15.0   # Angle at which the springs start to engage
 theta_max = 90.0    # Assumed angle at which maximum torque is achieved
 theta_max_rad = theta_max*np.pi/180.0
 torque_rating = 2000.0    # Spring torque rating, lbf*in
-kappa = torque_rating/theta_max_rad     # Torsional spring constant, (lbf*in/deg)
-weight = 600.0      # Door weight, lbs
+kappa = torque_rating/theta_max_rad     # Torsional spring constant, (lbf*in/rad)
+weight = 300.0      # Door weight, lbs (estimate 250)
 height = 45.0       # Height from pivot point to top of door
-nsprings = 4
+nsprings_initial = 2
+nsprings_dead = 4
 
 force = np.zeros(theta_open.shape)
 
 for i in range(len(theta_open)):
     angle = theta_open[i]
     if angle < theta_dead:
-        force[i] = (weight/2.0)*np.sin(angle*np.pi/180.0)
+        force[i] = (weight/2.0)*np.sin(angle*np.pi/180.0) - nsprings_initial*(kappa*(angle-theta_dead)*np.pi)/(180.0*height)
     else:
-        force[i] = (weight/2.0)*np.sin(angle*np.pi/180.0) - nsprings*(kappa*(angle-theta_dead)*np.pi)/(180.0*height)
+        force[i] = (weight/2.0)*np.sin(angle*np.pi/180.0) - (nsprings_initial+nsprings_dead)*(kappa*(angle-theta_dead)*np.pi)/(180.0*height)
 
 print("\nMaximum force, Concept 1: {} lbs".format(max(force)))
 
@@ -43,44 +44,79 @@ fig1 = plt.figure(1)
 plt.plot(theta_open,force)
 plt.xlabel("Angle (deg)")
 plt.ylabel("Force (lbf)")
-titlestring = "{} Torsion Springs, Dead Angle {} Degrees".format(nsprings, theta_dead)
+titlestring = "{} Total Springs, {} Initial, {} Added, Dead Angle {} Degrees".format(nsprings_initial + nsprings_dead, nsprings_initial, nsprings_dead, theta_dead)
 plt.title(titlestring)
 fig1.suptitle("Concept 1: Torsion Springs Only")
 # plt.show()
 
 
 
-
 ########## Pulley and cable option ##########
 P0 = 1.0    # Preload exerted by rotational spring on the door, lbf
-delta_p = 5.0                   # Pulley height above the door
+delta_p = -22.5                   # Pulley height relative to top of the door
 h_pulley = height + delta_p     # Pulley actual height
-L = height                      # Renaming door height to make equations short
-weight_rating = 150.0           # Weight rating of garage door spring
-nrotations = 8                  # Expected number of rotations (approx. # feet of garage door)
-theta_max_rad = 2*np.pi*nrotations
-R = 2.0     # Radius of rotational spring drum, in
-kappa = weight_rating*R / theta_max_rad
-# torque_rating = 9.5   # Spring torque rating, lbf*in
-# kappa = torque_rating/theta_max_rad     # lbf*in/rad
+cable_offset = 11.25
+L_cable = height - cable_offset     # Cable moment arm
+L_door = height     # Door height
+cm_factor = 2/3     # Factor for where center of mass of door is relative to pivot
+l_c_door = cm_factor*L_door # Moment arm for center of mass of door
+R = 4.5     # Radius of rotational spring drum, inches
 nsprings = 2
 
-force = np.zeros(theta_open.shape)
-
+# Determine the required spring constant to balance the door using two springs and the original concept
+kappa_necessary = np.zeros(theta_open.shape)
 for i in range(len(theta_open)):
     angle = theta_open[i]*np.pi/180.0
-    c = np.sqrt(h_pulley**2 + L**2 -2*h_pulley*L*np.cos(angle))
-    omega = c/R
-    P = P0 + nsprings*kappa*c/R
-    force[i] = (weight/2.0)*np.sin(angle) - P*np.sin(np.arcsin(h_pulley*np.sin(angle)/c))
-
-print("\nMaximum force, Concept 2: {} lbs".format(max(force)))
+    kappa_necessary[i] = (weight * l_c_door * np.sin(angle))/(nsprings*angle)
 
 fig2 = plt.figure(2)
+plt.plot(theta_open, kappa_necessary)   # Not sure this is calculated correctly, since the plot is not what I would expect
+plt.xlabel("Angle (deg)")
+plt.ylabel("Spring Constant (lbf*in)")
+titlestring = "Concept 1: Required Spring Constant, {} Springs".format(nsprings)
+plt.title(titlestring)
+
+
+# Plot the required spring constant per spring over the range of opening angles, cable concept
+kappa_necessary = np.zeros(theta_open.shape)
+for i in range(len(theta_open)):
+    angle = theta_open[i]*np.pi/180.0
+    c = np.sqrt(h_pulley**2 + l_c_door**2 - 2*h_pulley*l_c_door*np.cos(angle))
+    psi = np.arcsin((h_pulley*np.sin(angle)) / c)
+    omega = c/R
+
+    P = (weight * l_c_door * np.sin(angle))/(L_cable * np.sin(psi))
+    kappa_necessary[i] = (P * R**2)/(nsprings * c)      # Necessary spring constant per spring, lbf*in
+
+
+
+# Plot kappa_necessary over the range of angles for the cable concept
+fig3 = plt.figure(3)
+plt.plot(theta_open,kappa_necessary)
+plt.xlabel("Angle (deg)")
+plt.ylabel("Spring Constant (lbf*in)")
+titlestring = "Cable Concept Required Spring Constant, {} Springs".format(nsprings)
+plt.title(titlestring)
+
+
+# Calculate the force experienced by the operator using kappa_necessary springs
+force = np.zeros(theta_open.shape)
+for i in range(len(theta_open)):
+    angle = theta_open[i]*np.pi/180.0
+    c = np.sqrt(h_pulley**2 + l_c_door**2 - 2*h_pulley*l_c_door*np.cos(angle))
+    psi = np.arcsin((h_pulley*np.sin(angle)) / c)
+    kappa = max(kappa_necessary)
+    P = P0 + nsprings*c*kappa / R**2
+    force[i] = (1/L_door) * (weight*l_c_door*np.sin(angle) - P*L_cable*np.sin(psi))
+
+print(force)
+print("\nMaximum force, Concept 2: {} lbs".format(np.abs(max(force))))
+
+fig4 = plt.figure(4)
 plt.plot(theta_open,force)
 plt.xlabel("Angle (deg)")
-plt.ylabel("Force (lbf)")
-titlestring = "{} Torsion Springs".format(nsprings)
+plt.ylabel("Operator Force (lbf)")
+titlestring = "{} Torsion Springs, {} in-lbs each".format(nsprings, int(kappa))
 plt.title(titlestring)
-fig2.suptitle("Concept 2: Cables and Pulley")
+fig4.suptitle("Concept 2: Cables and Pulley")
 plt.show()
